dcdaa1e198354582b38aa91793edc413a720d698
Anirudh Choudhary <anirudhani.8889@gmail.com>
2024-05-14 19:29:30 +0530

    Add:Staking feature

---
+++ staking/src/components/ChangeStaking.jsx
@@ -0,0 +1,14 @@
+
+    <Stack
+      spacing={2}
+      direction="column"
+    >
+        <p>Stake Token</p>
+      <TextField
+        sx={{ width: 500 }}
+        id="outlined-number"
+        label="Enter Amount"
+        type="number"
+      />
+      <ColorButton variant="contained">Stake</ColorButton>
+    </Stack>
--- staking/src/pages/Staking.jsx
+++ staking/src/pages/Staking.jsx
@@ -3,12 +3,15 @@ import Button from '@mui/material/Button';
 import { grey } from '@mui/material/colors';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 
-export default function Staking () {
+export default function Staking() {
 
     const [connectedAddress, setConnectedAddress] = useState(null);
+    const [showStakeForm, setShowStakeForm] = useState(false);
 
-    const handleButtonClick = () => {
+    const handleButtonClick = () => 
+        {
         if (connectedAddress) {
             disconnectFromMetaMask();
         } else {
@@ -33,12 +36,35 @@ export default function Staking () {
         console.log("Disconnected from MetaMask");
     };
 
+
     return (
         <>
-            <Stack spacing={2} direction="row">
-                <ColorButton variant="contained"> Import Token To Wallet </ColorButton>
-                <ColorButton variant="contained" onClick={handleButtonClick}> {connectedAddress ? ` ${connectedAddress.substring(0, 5)}...${connectedAddress.substring(connectedAddress.length - 5)}` : "Connect Wallet"}  </ColorButton>
+            <Stack marginTop={3} spacing={5} direction="row" justifyContent="flex-end" paddingRight={5}>
+                <ColorButton variant="contained"> Add Token To Wallet </ColorButton>
+                <ColorButton variant="contained" onClick={handleButtonClick}>
+                    {connectedAddress ? ` ${connectedAddress.substring(0, 5)}...${connectedAddress.substring(connectedAddress.length - 5)}` : "Connect Wallet"}
+                </ColorButton>
             </Stack>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained" onClick={() => setShowStakeForm(true)}>Stake</Button>
+                    <Button variant="contained">Unstake</Button>
+                    <Button variant="contained">Claim Reward</Button>
+                </Stack>
+            </div>
+
+            {showStakeForm && (
+                <Stack spacing={2} direction="column" justifyContent="center" alignItems="center">
+                    <p>Stake Token</p>
+                    <TextField
+                        sx={{ width: 500 }}
+                        id="outlined-number"
+                        label="Enter Amount"
+                        type="number"
+                    />
+                    <StakeButton variant="contained">Stake</StakeButton>
+                </Stack>
+            )}           
         </>
     );
 }
@@ -47,6 +73,16 @@ const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(grey[900]),
     backgroundColor: grey[900],
     '&:hover': {
-      backgroundColor: grey[800],
-    }, 
-  }));
+        backgroundColor: grey[800],
+    },
+}));
+
+const StakeButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(grey[900]),
+    backgroundColor: grey[900],
+    '&:hover': {
+        backgroundColor: grey[800],
+    },
+    width: 500,
+    textTransform: 'none'
+}));
